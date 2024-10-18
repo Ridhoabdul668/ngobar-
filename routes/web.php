d884a99a5344f7db3737b36396f00e14de038f80
Ridhoabdul668 <isnanhafizd@gmail.com>
2024-10-18 09:14:58 +0700

    push ngobar

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+//ini adalah controler ngobar
+use App\Http\Controllers\NgobarController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+
+//ini contoh route memakai method get
+
+
+//Route::get('',function (){
+ //   return view ('ngobars.index');
+//  });
+
+
+
+
+Route::resource('ngobars',NgobarController::class);
